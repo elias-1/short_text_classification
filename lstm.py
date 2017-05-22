19d60ad0e876e43b6cfabbd800a0b17fb9cb8bff
Elias-masker <shileicao@stu.xjtu.edu.cn>
2017-05-22 10:48:49 +0800

    fix bug; add lstm

--- lstm.py
+++ lstm.py
@@ -4,12 +4,14 @@
 # Copyright (c) 2017 www.drcubic.com, Inc. All Rights Reserved
 #
 """
-File: cnn.py
+File: lstm.py
 Author: shileicao(shileicao@stu.xjtu.edu.cn)
-Date: 2017-5-22 08:43:08
+Date: 2017-5-22 10:44:14
 """
 
 from __future__ import absolute_import, division, print_function
+
+import os
 
 import numpy as np
 import tensorflow as tf
@@ -21,20 +23,18 @@ tf.app.flags.DEFINE_string('train_data_path', "data/train/normal_train.txt",
                            'Training data dir')
 tf.app.flags.DEFINE_string('test_data_path', "data/test/normal_test.txt",
                            'Test data dir')
-tf.app.flags.DEFINE_string('log_dir', "cnn_logs", 'The log  dir')
+tf.app.flags.DEFINE_string('log_dir', "lstm_logs", 'The log  dir')
 
 tf.app.flags.DEFINE_string("vocab_size", 879, "vocabulary size")
 tf.app.flags.DEFINE_integer("max_sentence_len", 20,
                             "max num of tokens per query")
-tf.app.flags.DEFINE_integer("embedding_size", 64, "second embedding size")
+tf.app.flags.DEFINE_integer("embedding_size", 100, "second embedding size")
+tf.app.flags.DEFINE_integer("num_hidden", 100, "hidden unit number")
 tf.app.flags.DEFINE_integer("batch_size", 64, "num example per mini batch")
 tf.app.flags.DEFINE_integer("train_steps", 2000, "trainning steps")
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate")
 
-tf.app.flags.DEFINE_string('filter_sizes', '3,4,5',
-                           'Comma-separated filter sizes (default: \'3,4,5\')')
-tf.app.flags.DEFINE_float("num_filters", 128, "Number of filters")
-tf.app.flags.DEFINE_float("num_classes", 14, "Number of classes to classify")
+tf.app.flags.DEFINE_float("num_classes", 7, "Number of classes to classify")
 tf.app.flags.DEFINE_float('dropout_keep_prob', 0.5,
                           'Dropout keep probability (default: 0.5)')
 
@@ -43,41 +43,15 @@ tf.flags.DEFINE_float('l2_reg_lambda', 0,
 
 
 class Model:
-    def __init__(self):
-        self.words = tf.Variable(
-            tf.random_uniform([FLAGS.vocab_size, FLAGS.embedding_size], -1.0,
-                              1.0),
-            name='words')
-
-        self.filter_sizes = list(map(int, FLAGS.filter_sizes.split(',')))
-
-        self.clfier_filters = [None] * len(self.filter_sizes)
-        self.clfier_bs = [None] * len(self.filter_sizes)
-        for i, filter_size in enumerate(self.filter_sizes):
-            with tf.variable_scope('Clfier_conv_maxpool') as scope:
-                self.clfier_filters[i] = tf.get_variable(
-                    "clfier_filter_%d" % i,
-                    shape=[
-                        filter_size, FLAGS.embedding_size, 1, FLAGS.num_filters
-                    ],
-                    regularizer=tf.contrib.layers.l2_regularizer(0.0001),
-                    initializer=tf.truncated_normal_initializer(stddev=0.01),
-                    dtype=tf.float32)
-
-                self.clfier_bs[i] = tf.get_variable(
-                    "clfier_b_%d" % i,
-                    shape=[FLAGS.num_filters],
-                    regularizer=tf.contrib.layers.l2_regularizer(0.0001),
-                    initializer=tf.truncated_normal_initializer(stddev=0.01),
-                    dtype=tf.float32)
+    def __init__(self, c2vPath, numHidden):
+        self.numHidden = numHidden
+        self.c2v = load_w2v(c2vPath, FLAGS.embedding_size)
+        self.chars = tf.Variable(self.c2v, name="chars")
 
         with tf.variable_scope('Clfier_output') as scope:
             self.clfier_softmax_W = tf.get_variable(
                 "clfier_W",
-                shape=[
-                    FLAGS.num_filters * len(self.filter_sizes),
-                    FLAGS.num_classes
-                ],
+                shape=[numHidden * 2, FLAGS.num_classes],
                 regularizer=tf.contrib.layers.l2_regularizer(0.0001),
                 initializer=tf.truncated_normal_initializer(stddev=0.01),
                 dtype=tf.float32)
@@ -89,48 +63,51 @@ class Model:
                 initializer=tf.truncated_normal_initializer(stddev=0.01),
                 dtype=tf.float32)
 
-        self.inp_w = tf.placeholder(
+        self.inp_c = tf.placeholder(
             tf.int32, shape=[None, FLAGS.max_sentence_len], name="input_words")
 
-    def inference(self, clfier_wX, trainMode=True):
-        word_vectors = tf.nn.embedding_lookup(self.words, clfier_wX)
-        word_vectors_expanded = tf.expand_dims(word_vectors, -1)
+    def length(self, data):
+        used = tf.sign(tf.abs(data))
+        length = tf.reduce_sum(used, reduction_indices=1)
+        length = tf.cast(length, tf.int32)
+        return length
 
-        pooled_outputs = []
+    def inference(self, clfier_cX, reuse=None, trainMode=True):
 
-        for i, filter_size in enumerate(self.filter_sizes):
-            conv = tf.nn.conv2d(
-                word_vectors_expanded,
-                self.clfier_filters[i],
-                strides=[1, 1, 1, 1],
-                padding='VALID')
-            # Apply nonlinearity
-            h = tf.nn.relu(tf.nn.bias_add(conv, self.clfier_bs[i]))
-            # Maxpooling over the outputs
-            pooled = tf.nn.max_pool(
-                h,
-                ksize=[1, FLAGS.max_sentence_len - filter_size + 1, 1, 1],
-                strides=[1, 1, 1, 1],
-                padding='VALID')
-            pooled_outputs.append(pooled)
+        char_vectors = tf.nn.embedding_lookup(self.chars, clfier_cX)
+        length = self.length(clfier_cX)
+        length_64 = tf.cast(length, tf.int64)
 
-        # Combine all the pooled features
-        num_filters_total = FLAGS.num_filters * len(self.filter_sizes)
-        clfier_pooled = tf.concat(pooled_outputs, 3)
-        clfier_pooled_flat = tf.reshape(clfier_pooled, [-1, num_filters_total])
+        # if trainMode:
+        #  char_vectors = tf.nn.dropout(char_vectors, FLAGS.dropout_keep_prob)
+        with tf.variable_scope("rnn_fwbw", reuse=reuse) as scope:
+            forward_output, _ = tf.nn.dynamic_rnn(
+                tf.contrib.rnn.LSTMCell(self.numHidden),
+                char_vectors,
+                dtype=tf.float32,
+                sequence_length=length,
+                scope="RNN_forward")
+            backward_output_, _ = tf.nn.dynamic_rnn(
+                tf.contrib.rnn.LSTMCell(self.numHidden),
+                inputs=tf.reverse_sequence(char_vectors, length_64, seq_dim=1),
+                dtype=tf.float32,
+                sequence_length=length,
+                scope="RNN_backword")
 
-        # Add dropout
+        backward_output = tf.reverse_sequence(
+            backward_output_, length_64, seq_dim=1)
+
+        output = tf.concat([forward_output, backward_output], 2)
         if trainMode:
-            with tf.name_scope('dropout'):
-                clfier_pooled_flat = tf.nn.dropout(clfier_pooled_flat,
-                                                   FLAGS.dropout_keep_prob)
+            output = tf.nn.dropout(output, FLAGS.dropout_keep_prob)
 
-        scores = tf.nn.xw_plus_b(clfier_pooled_flat, self.clfier_softmax_W,
+        ds = tf.reduce_sum(output, axis=1)
+        scores = tf.nn.xw_plus_b(ds, self.clfier_softmax_W,
                                  self.clfier_softmax_b)
         return scores
 
-    def loss(self, clfier_wX, clfier_Y):
-        self.scores = self.inference(clfier_wX)
+    def loss(self, clfier_cX, clfier_Y):
+        self.scores = self.inference(clfier_cX)
         cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
             logits=self.scores, labels=clfier_Y)
         loss = tf.reduce_mean(cross_entropy, name='cross_entropy')
@@ -141,7 +118,7 @@ class Model:
         return final_loss
 
     def test_clfier_score(self):
-        scores = self.inference(self.inp_w, trainMode=False)
+        scores = self.inference(self.inp_c, reuse=True, trainMode=False)
         return scores
 
 
@@ -177,9 +154,9 @@ def train(total_loss):
     return tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(total_loss)
 
 
-def test_evaluate(sess, test_clfier_score, inp_w, clfier_twX, clfier_tY):
+def test_evaluate(sess, test_clfier_score, inp_c, clfier_tcX, clfier_tY):
     batchSize = FLAGS.batch_size
-    totalLen = clfier_twX.shape[0]
+    totalLen = clfier_tcX.shape[0]
     numBatch = int((totalLen - 1) / batchSize) + 1
     correct_clfier_labels = 0
     for i in range(numBatch):
@@ -188,7 +165,7 @@ def test_evaluate(sess, test_clfier_score, inp_w, clfier_twX, clfier_tY):
             endOff = totalLen
         y = clfier_tY[i * batchSize:endOff]
         feed_dict = {
-            inp_w: clfier_twX[i * batchSize:endOff],
+            inp_c: clfier_tcX[i * batchSize:endOff],
         }
         clfier_score_val = sess.run([test_clfier_score], feed_dict)
         predictions = np.argmax(clfier_score_val[0], 1)
@@ -199,28 +176,57 @@ def test_evaluate(sess, test_clfier_score, inp_w, clfier_twX, clfier_tY):
 
 
 def main(unused_argv):
+    trainDataPath = FLAGS.train_data_path
     graph = tf.Graph()
+    # ner_checkpoint_file = tf.train.latest_checkpoint(FLAGS.ner_log_dir)
     with graph.as_default():
-        model = Model()
-        print("train data path:", FLAGS.train_data_path)
-        clfier_wX, clfier_Y = inputs(FLAGS.train_data_path)
-        clfier_twX, clfier_tY = load_data(FLAGS.test_data_path,
-                                          FLAGS.max_sentence_len)
-        total_loss = model.loss(clfier_wX, clfier_Y)
+        model = Model(FLAGS.char2vec_path, FLAGS.num_hidden)
+        print("train data path:", trainDataPath)
+        clfier_cX, clfier_Y = inputs(trainDataPath)
+        clfier_tcX, clfier_tY = do_load_data_char_common(
+            FLAGS.test_data_path, FLAGS.max_sentence_len)
+        total_loss = model.loss(clfier_cX, clfier_Y)
         train_op = train(total_loss)
         test_clfier_score = model.test_clfier_score()
 
+        # ner_variables = [
+        #     v for v in tf.global_variables() if 'rnn_fwbw' in v.name
+        # ] + [model.words, model.chars, model.char_filter]
+
+        # ner_saver = tf.train.Saver(ner_variables)
+
+        clfier_saver = tf.train.Saver(tf.global_variables())
+        if tf.gfile.Exists(FLAGS.clfier_log_dir):
+            tf.gfile.DeleteRecursively(FLAGS.clfier_log_dir)
+        tf.gfile.MakeDirs(FLAGS.clfier_log_dir)
+
+        clfier_checkpoint_path = os.path.join(FLAGS.clfier_log_dir,
+                                              'model.ckpt')
+
         gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.25)
-        sv = tf.train.Supervisor(graph=graph, logdir=FLAGS.log_dir)
-        with sv.managed_session(
-                master='',
-                config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+        with tf.Session(config=tf.ConfigProto(
+                gpu_options=gpu_options)) as sess:
+            # sv = tf.train.Supervisor(graph=graph, logdir=FLAGS.clfier_log_dir)
+            # with sv.managed_session(
+            #         master='',
+            #         config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+
+            # ner_saver.restore(sess, ner_checkpoint_file)
+            #
+            # other_variables = [
+            #     v for v in tf.global_variables() if v not in ner_variables
+            # ]
+            #
+            # init_op = tf.variables_initializer(other_variables)
+            init_op = tf.variables_initializer(tf.global_variables())
+            sess.run(init_op)
+            tf.train.start_queue_runners(sess=sess)
 
             # actual training loop
             training_steps = FLAGS.train_steps
             for step in range(training_steps):
-                if sv.should_stop():
-                    break
+                #                 if sv.should_stop():
+                #                     break
                 try:
                     _ = sess.run([train_op])
                     # for debugging and learning purposes, see how the loss gets decremented thru training steps
@@ -228,13 +234,19 @@ def main(unused_argv):
                         print("[%d] loss: [%r]" % (step + 1,
                                                    sess.run(total_loss)))
                     if (step + 1) % 20 == 0:
-                        test_evaluate(sess, test_clfier_score, model.inp_w,
-                                      clfier_twX, clfier_tY)
-                except KeyboardInterrupt as e:
-                    sv.saver.save(
-                        sess, FLAGS.log_dir + '/model', global_step=(step + 1))
+                        test_evaluate(sess, test_clfier_score, model.inp_c,
+                                      clfier_tcX, clfier_tY)
+                except KeyboardInterrupt, e:
+                    #     sv.saver.save(
+                    #         sess,
+                    #         FLAGS.clfier_log_dir + '/model',
+                    #         global_step=(step + 1))
+                    #     raise e
+                    # sv.saver.save(sess, FLAGS.clfier_log_dir + '/finnal-model')
+                    clfier_saver.save(
+                        sess, clfier_checkpoint_path, global_step=(step + 1))
                     raise e
-                sv.saver.save(sess, FLAGS.log_dir + '/finnal-model')
+            clfier_saver.save(sess, clfier_checkpoint_path)
 
 
 if __name__ == '__main__':
