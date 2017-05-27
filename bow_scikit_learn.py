28683c22993cb855e1613222d1fd072ef67adbd8
Elias-masker <shileicao@stu.xjtu.edu.cn>
2017-05-27 23:43:14 +0800

    add a simple BOW classifier using func provided by scikit-learn

---
+++ bow_scikit_learn.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 www.drcubic.com, Inc. All Rights Reserved
+#
+"""
+File: bow_scikit_learn.py
+Author: shileicao(shileicao@stu.xjtu.edu.cn)
+Date: 2017/5/27 22:56
+"""
+import os
+import sys
+
+import numpy as np
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+
+
+def stats_class(data_dir):
+    classes = {}
+    with open(os.path.join(data_dir, 'train.label'), 'r') as f:
+        for line in f.readlines():
+            if line.strip() not in classes:
+                classes[line.strip()] = len(classes)
+    return classes
+
+
+def build_dataset(data_dir, class_stats, train_or_test):
+    data_fp = open(os.path.join(data_dir, train_or_test + '.seq.in'), 'r')
+    label_fp = open(os.path.join(data_dir, train_or_test + '.label'), 'r')
+
+    data = []
+    y = []
+    while True:
+        sample_x = data_fp.readline().strip()
+        label = label_fp.readline().strip()
+        if not sample_x:
+            break
+        data.append(data)
+        y.append(class_stats[label])
+
+    data_fp.close()
+    label_fp.close()
+    return data, y
+
+
+def process_pipeline(train_x, train_y):
+    # count_vect = CountVectorizer()
+    # X_train_counts = count_vect.fit_transform(train_x)
+    # tfidf_transformer = TfidfTransformer(use_idf=False)
+    # X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    # clf = MultinomialNB().fit(X_train_tfidf, train_y)
+
+    text_clf = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        # ('clf', MultinomialNB()),
+        ('clf', SGDClassifier(
+            loss='hinge', penalty='l2', alpha=1e-3, n_iter=5,
+            random_state=42)),
+    ])
+    text_clf = text_clf.fit(train_x, train_y)
+
+
+def do_test(text_clf, test_x, test_y, test_label_names):
+    predicted = text_clf.predict(test_x)
+    print(np.mean(predicted == test_y))
+
+    print(metrics.classification_report(
+        test_y, predicted, target_names=test_label_names))
+    metrics.confusion_matrix(test_y, predicted)
+
+
+def grid_search(text_clf, train_x, train_y):
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2)],
+        'tfidf__use_idf': (True, False),
+        'clf__alpha': (1e-2, 1e-3),
+    }
+    gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
+    gs_clf = gs_clf.fit(train_x[:400], train_y[:400])
+    print(gs_clf.best_score_)
+    for param_name in sorted(parameters.keys()):
+        print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
+
+    # print(gs_clf.cv_results_)
+
+
+def main(argc, argv):
+    if argc < 3:
+        print('Usage:%s <train_dir> <test_dir> <cv>' % (argv[0]))
+        exit(1)
+
+    class_stats = stats_class(argv[1])
+    train_x, train_y = build_dataset(argv[1], class_stats, 'train')
+    test_x, test_y = build_dataset(argv[2], class_stats, 'test')
+    process_pipeline(train_x, train_y)
+
+
+if __name__ == '__main__':
+    main(len(sys.argv), sys.argv)
