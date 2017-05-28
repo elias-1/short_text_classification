f8a9a6181e961ecbeea1499c0b12090f20935686
Elias-masker <shileicao@stu.xjtu.edu.cn>
2017-05-28 23:30:03 +0800

    refine code;

--- bow_scikit_learn.py
+++ bow_scikit_learn.py
@@ -40,7 +40,7 @@ def build_dataset(data_dir, class_stats, train_or_test):
         label = label_fp.readline().strip()
         if not sample_x:
             break
-        data.append(data)
+        data.append(sample_x)
         y.append(class_stats[label])
 
     data_fp.close()
@@ -48,7 +48,11 @@ def build_dataset(data_dir, class_stats, train_or_test):
     return data, y
 
 
-def process_pipeline(train_x, train_y):
+def my_tokenizer(s):
+    return s.split(' ')
+
+
+def process_pipeline():
     # count_vect = CountVectorizer()
     # X_train_counts = count_vect.fit_transform(train_x)
     # tfidf_transformer = TfidfTransformer(use_idf=False)
@@ -56,14 +60,13 @@ def process_pipeline(train_x, train_y):
     # clf = MultinomialNB().fit(X_train_tfidf, train_y)
 
     text_clf = Pipeline([
-        ('vect', CountVectorizer()),
+        ('vect', CountVectorizer(tokenizer=my_tokenizer)),
         ('tfidf', TfidfTransformer()),
         # ('clf', MultinomialNB()),
         ('clf', SGDClassifier(
-            loss='hinge', penalty='l2', alpha=1e-3, n_iter=5,
-            random_state=42)),
+            loss='hinge', penalty='l2', n_iter=5, random_state=42)),
     ])
-    text_clf = text_clf.fit(train_x, train_y)
+    return text_clf
 
 
 def do_test(text_clf, test_x, test_y, test_label_names):
@@ -82,23 +85,31 @@ def grid_search(text_clf, train_x, train_y):
         'clf__alpha': (1e-2, 1e-3),
     }
     gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
-    gs_clf = gs_clf.fit(train_x[:400], train_y[:400])
+    gs_clf = gs_clf.fit(train_x, train_y)
     print(gs_clf.best_score_)
     for param_name in sorted(parameters.keys()):
         print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
 
     # print(gs_clf.cv_results_)
+    return gs_clf
 
 
 def main(argc, argv):
     if argc < 3:
-        print('Usage:%s <train_dir> <test_dir> <cv>' % (argv[0]))
+        print('Usage:%s <train_dir> <test_dir>' % (argv[0]))
         exit(1)
 
     class_stats = stats_class(argv[1])
     train_x, train_y = build_dataset(argv[1], class_stats, 'train')
     test_x, test_y = build_dataset(argv[2], class_stats, 'test')
-    process_pipeline(train_x, train_y)
+
+    text_clf = process_pipeline()
+    gs_clf = grid_search(text_clf, train_x, train_y)
+    int2class = {}
+    for key in class_stats.keys():
+        int2class[class_stats[key]] = key
+    test_name = [int2class[y] for y in test_y]
+    do_test(gs_clf, test_x, test_y, test_name)
 
 
 if __name__ == '__main__':
